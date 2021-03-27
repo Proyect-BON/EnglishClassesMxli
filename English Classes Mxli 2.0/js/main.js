6f094738385033171c8685a3cdf115c9c2b693b6
B.O.N <79944431+Proyect-BON@users.noreply.github.com>
2021-03-26 17:50:40 -0700

    Add files via upload

---
+++ English Classes Mxli 2.0/js/main.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.main');
+ScrollReveal().reveal('.info', {delay: 500});
+ScrollReveal().reveal('.info1', {delay: 500});
+ScrollReveal().reveal('.info2', {delay: 2000});
+ScrollReveal().reveal('.banner1', {delay: 500});
+ScrollReveal().reveal('.info2-0', {delay: 500});
+ScrollReveal().reveal('.banner2', {delay: 500});
+ScrollReveal().reveal('.maestros', {delay: 500});
+ScrollReveal().reveal('.social', {delay: 100});
